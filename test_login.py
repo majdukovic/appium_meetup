40f9708f44a847fd6949fe747b59c53a65dbff7a
mate_xanadu <majdukovic@xanadu.ie>
2018-07-04 14:59:48 +0100

    Added PIN login with Browserstack

--- test_login.py
+++ test_login.py
@@ -6,53 +6,56 @@ class TestLogin():
 
     # Set desired capabilities
     desired_caps = {}
-    desired_caps['app'] = '/Users/teammatchbook/mate_appium/MB_4_1_1_app.apk'
+    desired_caps['app'] = 'app_location' #change (credentials.txt)
     desired_caps['platformName'] = 'Android'
     desired_caps['platformVersion'] = '7.0'
-    #desired_caps['deviceName'] = 'Xiaomi Redmi Note 4'
-    desired_caps['deviceName'] = 'Galaxy Nexus 7.0.0'
-    desired_caps['avd'] = 'Galaxy_Nexus_7.0.0'
+    desired_caps['deviceName'] = 'Samsung Galaxy S8'
     desired_caps['appWaitPackage'] = 'com.matchbook.client'
     desired_caps['appWaitActivity'] = 'com.android.xanadu.matchbook.MainActivity'
     desired_caps['autoGrantPermissions'] = True
+    desired_caps['browserstack.local'] = True
+    desired_caps['browserstack.idleTimeout'] = '10'
 
     def setup(self):
         # Start webdriver
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', self.desired_caps)
+        self.driver = webdriver.Remote('server_location', self.desired_caps) #change (credentials.txt)
         self.driver.implicitly_wait(10)
 
     def test_01_login(self):
         # Execute test
-        time.sleep(5)
         self.driver.find_element_by_id('com.matchbook.client:id/tvButton').click()
-        time.sleep(3)
+        #time.sleep(2)
         self.driver.find_element_by_id('com.matchbook.client:id/loginButton').click()
-        time.sleep(2)
+        #time.sleep(2)
         self.driver.find_element_by_id('com.matchbook.client:id/editTextUsername').click()
-        self.driver.find_element_by_id('com.matchbook.client:id/editTextUsername').send_keys('username')
+        self.driver.find_element_by_id('com.matchbook.client:id/editTextUsername').send_keys('majdukovic')
         self.driver.find_element_by_id('com.matchbook.client:id/editTextPassword').click()
-        time.sleep(1)
-        self.driver.find_element_by_id('com.matchbook.client:id/editTextPassword').send_keys('password')
+        #time.sleep(1)
+        self.driver.find_element_by_id('com.matchbook.client:id/editTextPassword').send_keys('pass1234')
         self.driver.hide_keyboard()
-        time.sleep(2)
+        #time.sleep(2)
         self.driver.find_element_by_id('com.matchbook.client:id/buttonLogIn').click()
-        time.sleep(10)
+        #time.sleep(2)
         self.driver.find_element_by_id('com.matchbook.client:id/activatePin').click()
         self.driver.find_element_by_id('com.matchbook.client:id/pin1').send_keys('1111')
-        time.sleep(1)
+        #time.sleep(1)
         self.driver.find_element_by_id('com.matchbook.client:id/pin1').send_keys('1111')
-        time.sleep(2)
+        #time.sleep(2)
         self.driver.find_element_by_id('com.matchbook.client:id/pinPwd').click()
-        self.driver.find_element_by_id('com.matchbook.client:id/pinPwd').send_keys('password')
-        time.sleep(2)
+        self.driver.find_element_by_id('com.matchbook.client:id/pinPwd').send_keys('pass1234')
+        #time.sleep(2)
         self.driver.find_element_by_id('com.matchbook.client:id/okButton').click()
-        time.sleep(8)
+        #time.sleep(2)
         self.driver.find_element_by_id('com.matchbook.client:id/okButton').click()
-        time.sleep(1)
+        #time.sleep(1)
+        self.driver.find_element_by_id('com.matchbook.client:id/ivAccount').click()
+        self.driver.find_element_by_id('com.matchbook.client:id/ivLogout').click()
+        self.driver.find_element_by_id('com.matchbook.client:id/loginButton').click()
+        self.driver.find_element_by_id('com.matchbook.client:id/pin1').send_keys('1111')
         assert_true(self.driver.find_element_by_id('com.matchbook.client:id/ivAccount').is_displayed(),
         "Account button is not displayed, user is not logged in.")
-        print "User is logged in."
-        time.sleep(5)
+        print "User is logged in with PIN."
+        time.sleep(2)
 
     def teardown(self):
         # Quit session
